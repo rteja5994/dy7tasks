014a3c133716c260daa27531743e50b00e3d900e
rteja5994 <raviteja.chunduri@dal.ca>
2018-01-22 16:28:44 -0400

    find_centre and move_rect

--- task2.py
+++ task2.py
@@ -7,7 +7,7 @@ class Point():
 class Rectangle():
     width = 0
     height = 0
-
+    corner = Point()
 
     def __init__(self, initx, inity):
         self.width = initx
@@ -18,9 +18,15 @@ class Rectangle():
         centre = ((corner.x+self.height)/2 , (corner.y+self.width)/2)
         return centre
 
+    def move_rectangle(self,dx, dy):
+        ect = ((self.corner.x + dx), (self.corner.y + dy))
+        return ect
+
 p= Point()
 p.x = 10
 p.y = 20
 r= Rectangle(100,200)
+r.corner = p
 
 print(r.find_centre(p))
+print(r.move_rectangle(5,5))
